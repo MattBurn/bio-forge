3e086b2c8500568940d81688b5e6a5bea05bf742
Tony Kan <tianchengkan@gmail.com>
2025-11-23 22:17:28 -0800

    refactor(lib): Change module declarations to private for improved encapsulation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
-pub mod db;
+mod db;
+mod model;
+
 pub mod io;
-pub mod model;
 pub mod ops;
+
+pub use crate::model::atom::Atom;
+pub use crate::model::chain::Chain;
+pub use crate::model::residue::Residue;
+pub use crate::model::structure::Structure;
+pub use crate::model::template::Template;
+pub use crate::model::topology::{Bond, Topology};
+pub use crate::model::types::{
+    BondOrder, Element, Point, ResidueCategory, ResiduePosition, StandardResidue,
+};
