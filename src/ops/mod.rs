b1f7c5c9693e46291f0d2440223265ac5ee7ff4a
Tony Kan <tianchengkan@gmail.com>
2025-11-23 22:17:13 -0800

    refactor(ops): Change module declarations to private for improved encapsulation

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,7 +1,21 @@
-pub mod clean;
-pub mod error;
-pub mod hydro;
-pub mod repair;
-pub mod solvate;
-pub mod topology;
-pub mod transform;
+mod clean;
+mod error;
+mod hydro;
+mod repair;
+mod solvate;
+mod topology;
+mod transform;
+
+pub use clean::{CleanConfig, clean_structure};
+
+pub use repair::repair_structure;
+
+pub use hydro::{HisStrategy, HydroConfig, add_hydrogens};
+
+pub use solvate::{Anion, Cation, SolvateConfig, solvate_structure};
+
+pub use transform::Transform;
+
+pub use topology::TopologyBuilder;
+
+pub use error::Error;
