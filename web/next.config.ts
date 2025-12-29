e187d619bef725207d06adc7a22caef97d869c72
Tony Kan <tianchengkan@gmail.com>
2025-12-29 04:36:48 -0800

    chore(web): Update next.config.ts to support static export and base path configuration

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  ...(process.env.STATIC_EXPORT === "true" && {
+    output: "export",
+    images: { unoptimized: true },
+  }),
+  ...(process.env.BASE_PATH && {
+    basePath: process.env.BASE_PATH,
+    assetPrefix: `${process.env.BASE_PATH}/`,
+  }),
+
   webpack: (config) => {
     config.experiments = {
       ...config.experiments,
